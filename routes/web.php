f4278a7c2daa1583c34c2a031be91855b8eb3304
Jade <862226994@qq.com>
2018-02-08 14:39:07 +0800

    xg

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,12 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['namespace' => 'Wechat', 'prefix' => 'wechat'], function(){
+Route::group(['middleware' => ['web', 'wechat.oauth'],'namespace' => 'Wechat', 'prefix' => 'wechat'], function(){
     Route::any('/wechat', 'WeChatController@serve');
+    Route::get('/user', function () {
+        $user = session('wechat.oauth_user'); // 拿到授权用户资料
+
+        dd($user);
+    });
 });
 
