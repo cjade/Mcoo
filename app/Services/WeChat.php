11917b51f69a48a015b70a3b37c781ed201d5464
Jade <862226994@qq.com>
2018-02-11 12:57:41 +0800

    ai

--- app/Services/WeChat.php
+++ app/Services/WeChat.php
@@ -141,7 +141,7 @@ class WeChat
                 // 执行API调用
                 $url = 'https://api.ai.qq.com/fcgi-bin/nlp/nlp_texttrans';
                 $response = Ai::doHttpPost($url, $params);
-                Log::info(json_decode($response)->msg);
+                Log::info(json_decode($response));
                 return json_decode($response)->data->trans_text;
                 break;
         }
